ca615f89bac61ffe3a34acd07c9a20b9601a4472
rwestbrookjr <ron.westbrook.jr@gmail.com>
2021-11-14 00:47:01 -0600

    End of Basics

---
+++ src/books.js
@@ -0,0 +1,26 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91PpDVWjhLL._AC_UL200_SR200,200_.jpg',
+    title: 'Will',
+    author: 'Will Smith',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91DEviT3oUL._AC_UL200_SR200,200_.jpg',
+    title: 'How to Catch a Turkey',
+    author: 'Adam Wallace',
+  },
+  {
+    id: 4,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+  },
+]
